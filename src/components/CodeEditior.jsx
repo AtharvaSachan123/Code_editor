35a87a07d14e1c2b632b9772046fa7963cea36ed
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-04-17 23:44:12 +0530

    Layout correction

--- src/components/CodeEditior.jsx
+++ src/components/CodeEditior.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@chakra-ui/react'
+import { Box, HStack } from '@chakra-ui/react'
 import React from 'react'
 import { Editor } from '@monaco-editor/react'
 import { useState } from 'react'
@@ -12,28 +12,40 @@ export const CodeEditior = () => {
 
 
     const onMount = (editor) => {
-    editorRef.current = editor;
-    editor.focus();
-    
+        editorRef.current = editor;
+        editor.focus();
+
     }
-    const onSelect=(language)=>{
+    const onSelect = (language) => {
         setLanguage(language);
         setValue(
             CODE_SNIPPETS[language],
         )
     }
-  return (
-    <Box>
-        <LanguageSelector language={language} onSelect={onSelect}/>
-        <Editor height="75vh" 
-            theme='vs-dark'
-            language={language}
-         defaultValue={CODE_SNIPPETS[language]}
-            onMount={onMount}
-         value={value} 
-         onChange={
-            (value) => setValue(value)
-         }/>
-    </Box>
-  )
+    return (
+        <Box>
+            <HStack spacing={4}>
+                <Box w='50%'>
+
+                    <LanguageSelector language={language} onSelect={onSelect} />
+                    <Editor height="75vh"
+                        theme='vs-dark'
+                        language={language}
+                        defaultValue={CODE_SNIPPETS[language]}
+                        onMount={onMount}
+                        value={value}
+                        onChange={
+                            (value) => setValue(value)
+                        } />
+
+                </Box>
+
+
+            </HStack>
+
+
+
+
+        </Box>
+    )
 }
